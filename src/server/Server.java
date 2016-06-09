9cf88979cd5309e04c6eb0837da3e92942cd75cb
Revers <Revers@Hinata>
2016-06-09 15:01:00 +0200

    zmiana portów

--- src/server/Server.java
+++ src/server/Server.java
@@ -8,6 +8,7 @@ package server;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import static java.lang.Thread.sleep;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -19,8 +20,8 @@ public class Server {
     public static void main(String argv[]) throws Exception {
         String clientSentence;
         String capitalizedSentence;
-        ServerSocket welcomeSocket = new ServerSocket(8080);
-        while (true) {
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+        while (true) {        
             Socket connectionSocket = welcomeSocket.accept();
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
